01a633c1f4cfa97e158284ab03ff200b50dc971a
basp91 <basp91@gmail.com>
2015-09-12 15:30:19 -0500

    Commit inicial

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=diputados',
+    'username' => 'diputados',
+    'password' => 'YUhGMYL2uHLK8jt6',
+    'charset' => 'utf8',
+];
